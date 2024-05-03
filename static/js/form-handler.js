12fa9aa1153c12b94f457019d84955182f16398c
Taline <taline.zeidan@hotmail.com>
2024-05-03 22:33:00 +0300

    Added to templates and static

---
+++ static/js/form-handler.js
@@ -0,0 +1,6 @@
+function handleSubmit(event) {
+    event.preventDefault(); 
+    const form = document.querySelector('.signup-form');
+    form.submit(); 
+    window.location.href = 'C:\Users\Nour Rajeh\Desktop\Leafcards\src\leafsent.html'; // redirect to leefsent
+}
